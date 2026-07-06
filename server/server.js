bf6e70c74a671282f95350183432ceaca17e0b50
Aditya Damodar <adityardamodar@gmail.com>
2026-07-06 16:16:00 +0530

    Fix CORS for production

--- server/server.js
+++ server/server.js
@@ -14,7 +14,15 @@ connectDB();
 
 const app = express();
 
-app.use(cors({origin: ["http://localhost:5173",],credentials: true,}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://leave-flow-iota.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
